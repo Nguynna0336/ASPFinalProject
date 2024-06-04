b39888ffd432319713d3666d2d57343e0fd7225b
nken <nken3820@gmai.com>
2024-06-04 11:20:13 +0700

    connect DB

--- ASPFinalProject/Program.cs
+++ ASPFinalProject/Program.cs
@@ -1,4 +1,99 @@
-﻿using ASPFinalProject.Models;
+﻿// using ASPFinalProject.Models;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.EntityFrameworkCore;
+
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// var configuration = builder.Configuration;
+// // Add services to the container.
+// builder.Services.AddControllersWithViews();
+// /*builder.Services.AddDbContext<ExamDbContext>(options =>
+// {
+//     options.UseMySQL(
+//         configuration["ConnectionStrings:DefaultConnection"],
+//         new MySqlServerVersion(new Version(8, 0, 13))
+//         );
+// });*/
+// builder.Services.AddDbContext<ExamDbContext>(options =>
+//     options.UseMySql(
+//         builder.Configuration.GetConnectionString("DefaultConnection"),
+//         new MySqlServerVersion(new Version(8, 0, 26)) // Thay phiên bản MySQL của bạn ở đây
+//     )
+// );
+// builder.Services.AddIdentity<User, Role>(options =>
+// {
+//     options.Password.RequiredLength = 8;
+
+//     options.Lockout.MaxFailedAccessAttempts = 5;
+// })
+//     .AddEntityFrameworkStores<ExamDbContext>()
+//     .AddDefaultTokenProviders()
+//     .AddRoles<Role>();
+    
+
+// builder.Services.ConfigureApplicationCookie(options =>
+// {
+//     options.LoginPath = "/User/Login";
+//     options.AccessDeniedPath = "/";
+//     options.LogoutPath = "/";
+//     options.ExpireTimeSpan = TimeSpan.FromDays(1);
+//     options.SlidingExpiration = true;
+// });
+// builder.Services.AddAuthorization(options =>
+// {
+//     options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+// });
+
+
+// var app = builder.Build();
+
+// await CreateRoles(app.Services);
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// app.Run();
+
+// async Task CreateRoles(IServiceProvider serviceProvider)
+// {
+//     using (var scope = serviceProvider.CreateScope())
+//     {
+//         var roleManager = serviceProvider.GetRequiredService<RoleManager<Role>>();
+//         string[] roleNames = { "Admin", "Student", "Teacher" };
+//         IdentityResult roleResult;
+
+//         foreach (var roleName in roleNames)
+//         {
+//             var roleExist = await roleManager.RoleExistsAsync(roleName);
+//             if (!roleExist)
+//             {
+//                 roleResult = await roleManager.CreateAsync(new Role() { Name = roleName });
+//             }
+//         }
+
+//     }
+// }
+
+
+using ASPFinalProject.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +122,7 @@ builder.Services.AddIdentity<User, Role>(options =>
 
     options.Lockout.MaxFailedAccessAttempts = 5;
 })
+    .AddRoles<Role>()
     .AddEntityFrameworkStores<ExamDbContext>()
     .AddDefaultTokenProviders();
 
@@ -72,16 +168,20 @@ app.Run();
 
 async Task CreateRoles(IServiceProvider serviceProvider)
 {
-    var roleManager = serviceProvider.GetRequiredService<RoleManager<Role>>();
-    string[] roleNames = { "Admin", "Student", "Teacher" };
-    IdentityResult roleResult;
-
-    foreach (var roleName in roleNames)
+    using(var scope = serviceProvider.CreateScope())
     {
-        var roleExist = await roleManager.RoleExistsAsync(roleName);
-        if(!roleExist)
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
+        string[] roleNames = { "Admin", "Student", "Teacher" };
+        IdentityResult roleResult;
+
+        foreach (var roleName in roleNames)
         {
-            roleResult = await roleManager.CreateAsync(new Role() { Name = roleName });
+            var roleExist = await roleManager.RoleExistsAsync(roleName);
+            if (!roleExist)
+            {
+                roleResult = await roleManager.CreateAsync(new Role() { Name = roleName });
+            }
         }
     }
+
 }
